06a8280a054235a814d104aa2762cd2a350c816e
Nikolas9896 <nhlynka@gmail.com>
2018-11-13 13:14:42 +0200

    Task2. Condition

---
+++ Factorial_Task2_HlynkaMykola.java
@@ -0,0 +1,17 @@
+/*
+
+*******************************Task 2: The Factorial**************************
+Given the value of n! (n! = 1 * 2 * ... * (n - 1) * n). Find the value of n.
+
+Input
+The value of n! (1 ≤ n ≤ 2000).
+
+Output
+Print the value of positive integer n.
+
+Input example #1
+5040
+
+Output example #1
+7
+*/
