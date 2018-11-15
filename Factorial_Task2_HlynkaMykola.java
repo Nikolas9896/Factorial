bf11136fd71d410731878a466f7518cca006fdeb
Nikolas9896 <nhlynka@gmail.com>
2018-11-15 10:59:43 +0200

    update function multiply(int x, int res[], int res_size)

--- Factorial_Task2_HlynkaMykola.java
+++ Factorial_Task2_HlynkaMykola.java
@@ -66,7 +66,6 @@ class LargeFactorial {
 			int res_size = 1;
 			for (int x = 2; x <= n; x++) 
 			{
-				System.out.print(" X: " +x );
 				res_size = multiply(x, res, res_size);
 				if( res_size == digits)
 				{	
@@ -102,7 +101,30 @@ class LargeFactorial {
     // and returns the new value of res_size 
     static int multiply(int x, int res[], int res_size) 
     { 
-        return 0;
+        int carry = 0; // Initialize carry 
+  
+        // One by one multiply n with individual  
+        // digits of res[] 
+        for (int i = 0; i < res_size; i++) 
+        { 
+            int prod = res[i] * x + carry; 
+            res[i] = prod % 10; // Store last digit of  
+                                // 'prod' in res[] 
+            carry = prod/10; // Put rest in carry
+			
+			
+        } 
+  
+        // Put carry in res and increase result size 
+        while (carry!=0) 
+        { 
+            res[res_size] = carry % 10; 
+            carry = carry / 10; 
+            res_size++;
+					
+        } 
+		
+        return res_size;
     } 
 	
 
